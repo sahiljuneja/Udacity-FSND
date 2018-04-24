eae3dc664ced3dbb98c5d37ddf51686a23ba30a9
Sahil Juneja <sahiljuneja@users.noreply.github.com>
2018-04-24 18:16:52 +0530

    add docstring

--- P1-Movie-Trailer-Website/media.py
+++ P1-Movie-Trailer-Website/media.py
@@ -1,5 +1,15 @@
 class Movie:
-	""" docstring """
+	""" Movie class docstring
+
+	This class is used to create an object to store movie related information
+
+	Attributes:
+		title - String to store movie title.
+		movie_description - String to store movie description.
+		poster_image_url - String to store URL of movie's poster.
+		trailer_youtube_url - String to store youtube URL of movie's trailer.
+		
+	"""
 	def __init__(self, title, movie_description, poster_image_url, trailer_youtube_url):
 		self.title = title
 		self.movie_description = movie_description
