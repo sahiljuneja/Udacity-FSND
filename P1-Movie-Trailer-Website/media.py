88ac6735cbbc51dfb76c6e47e7e4a1a76978c084
Sahil Juneja <sahiljuneja@users.noreply.github.com>
2018-04-24 20:40:19 +0530

    update to PEP8 standards

--- P1-Movie-Trailer-Website/media.py
+++ P1-Movie-Trailer-Website/media.py
@@ -1,17 +1,19 @@
 class Movie:
-	""" Movie class docstring
+    """ Movie class docstring
 
-	This class is used to create an object to store movie related information
+    This class is used to create an object to store movie related information
 
-	Attributes:
-		title - String to store movie title.
-		movie_description - String to store movie description.
-		poster_image_url - String to store URL of movie's poster.
-		trailer_youtube_url - String to store youtube URL of movie's trailer.
-		
-	"""
-	def __init__(self, title, movie_description, poster_image_url, trailer_youtube_url):
-		self.title = title
-		self.movie_description = movie_description
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
+    Attributes:
+        title - String to store movie title.
+        movie_description - String to store movie description.
+        poster_image_url - String to store URL of movie's poster.
+        trailer_youtube_url - String to store youtube URL of movie's trailer.
+
+    """
+
+    def __init__(self, title, movie_description, poster_image_url,
+    	         trailer_youtube_url):
+        self.title = title
+        self.movie_description = movie_description
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
