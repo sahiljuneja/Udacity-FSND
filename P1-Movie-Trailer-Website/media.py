28a0626711dbb23ce327d29bbed6d0834f6701d6
Sahil Juneja <sahiljuneja@users.noreply.github.com>
2018-04-24 16:20:53 +0530

    add media.py

---
+++ P1-Movie-Trailer-Website/media.py
@@ -0,0 +1,7 @@
+Class Movie():
+	""" docstring """
+	def __init__(self, movie_title, movie_description, movie_poster_url, movie_video_url):
+		self.movie_title = movie_title
+		self.movie_description = movie_description
+		self.movie_poster_url = movie_poster_url
+		self.movie_video_url = movie_video_url
